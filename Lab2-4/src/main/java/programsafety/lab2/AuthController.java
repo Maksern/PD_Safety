a803e531edd3f35df3a97afb97bd06cd220973a6
Maksern <maks.men9922@gmail.com>
2024-05-10 17:11:24 +0300

    Add login and signup pages. Releaze token validation.

---
+++ Lab2-4/src/main/java/programsafety/lab2/AuthController.java
@@ -0,0 +1,125 @@
+package programsafety.lab2;
+
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.JwkException;
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.UrlJwkProvider;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.RSAKeyProvider;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Controller
+public class AuthController {
+    public Map<String, String> session;
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.session = new HashMap<>();
+        this.authService = authService;
+    }
+
+    @GetMapping("/*")
+    public String home(Model model) {
+        if(session.get("username") != null){
+            model.addAttribute("username", session.get("username"));
+            model.addAttribute("header", session.get("header"));
+            model.addAttribute("payload", session.get("payload"));
+        }
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String login(Model model) {
+        if(session.get("message") != null){
+            model.addAttribute("message", session.get("message"));
+        }
+        return "loginPage";
+    }
+
+    @GetMapping("/login")
+    public String logIn(@RequestParam String username, @RequestParam String pass) {
+        HttpResponse<JsonNode> response = authService.logIn(username, pass);
+
+        if (response.getStatus() == 200) {
+            String access_token = (String) response.getBody().getObject().get("access_token");
+            String refresh_token = (String) response.getBody().getObject().get("refresh_token");
+            authService.addParamsToSession(session, access_token, refresh_token, username);
+
+            return "redirect:/home";
+        }
+
+        session.put("message", response.getBody().getObject().get("error_description").toString());
+
+        return "redirect:/loginPage";
+    }
+
+    @GetMapping("/signupPage")
+    public String signup(Model model) {
+        if(session.get("message") != null){
+            model.addAttribute("message", session.get("message"));
+        }
+        return "signupPage";
+    }
+
+    @GetMapping("/signup")
+    public String signUp(@RequestParam String username, @RequestParam String pass) {
+        HttpResponse<JsonNode> response = authService.signUp(username, pass);
+
+        if (response.getStatus() == 201) {
+            session.put("message", "Success create new user");
+            return "redirect:/loginPage";
+        }
+
+        session.put("error", response.getBody().getObject().get("error_description").toString());
+
+        return "redirect:/signupPage";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.clear();
+        return "redirect:/home";
+    }
+
+    @GetMapping("/updateToken")
+    public String updateToken() {
+        HttpResponse<JsonNode> response = authService.updateTokenWithRefresh(session.get("refresh_token"));
+
+        if (response.getStatus() == 200) {
+            String access_token = (String) response.getBody().getObject().get("access_token");
+            authService.updateAccesTokenInSession(session, access_token);
+        } else {
+            session.put("error", response.getBody().getObject().get("error_description").toString());
+        }
+
+        return "redirect:/home";
+    }
+
+    @GetMapping("/validateToken")
+    @ResponseBody
+    public String validateToken(@RequestParam(defaultValue = "") String addToToken ) throws JwkException {
+        DecodedJWT jwt = JWT.decode(session.get("access_token") + addToToken);
+        JwkProvider jwkProvider = new UrlJwkProvider("https://dev-l5yejihpj316wdj1.us.auth0.com/");
+        Jwk jwk = jwkProvider.get(jwt.getKeyId());
+        Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+
+        try {
+            algorithm.verify(jwt);
+            return "Token is valid";
+        } catch (Exception exception) {
+            return "Token is invalid";
+        }
+    }
+}
---
+++ Lab2-4/src/main/java/programsafety/lab2/AuthService.java
@@ -0,0 +1,134 @@
+package programsafety.lab2;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.Map;
+
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private Environment env;
+
+    public HttpResponse<JsonNode> logIn(String username, String pass) {
+        String body = String.format("grant_type=password&" +
+                "username=%s&" +
+                "password=%s&" +
+                "scope=offline_access&" +
+                "client_id=%s&" +
+                "client_secret=%s&" +
+                "audience=%s", username, pass,
+                env.getProperty("okta.oauth2.client-id"), env.getProperty("okta.oauth2.client-secret"), env.getProperty("okta.oauth2.audience"));
+
+        try {
+            HttpResponse<JsonNode> response = Unirest.post("https://dev-l5yejihpj316wdj1.us.auth0.com/oauth/token")
+                    .header("content-type", "application/x-www-form-urlencoded")
+                    .body(body)
+                    .asJson();
+
+            return response;
+
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public HttpResponse<JsonNode> signUp(String username, String pass) {
+        String access_token = getAppAccessToken();
+        String body = String.format("connection=Username-Password-Authentication&" +
+                        "email=%s&" + "password=%s&" + "username=%s",
+                        username, pass, username.split("@")[0]);
+        System.out.println(body);
+        try {
+            HttpResponse<JsonNode> response = Unirest.post("https://dev-l5yejihpj316wdj1.us.auth0.com/api/v2/users")
+                    .header("content-type", "application/x-www-form-urlencoded")
+                    .header("Authorization", String.format("Bearer %s", access_token))
+                    .body(body)
+                    .asJson();
+
+            return response;
+
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getAppAccessToken() {
+        String body = String.format("grant_type=client_credentials&" +
+                        "client_id=%s&" +
+                        "client_secret=%s&" +
+                        "audience=%s",
+                env.getProperty("okta.oauth2.client-id"), env.getProperty("okta.oauth2.client-secret"), env.getProperty("okta.oauth2.audience"));
+
+        try {
+            HttpResponse<JsonNode> response = Unirest.post("https://dev-l5yejihpj316wdj1.us.auth0.com/oauth/token")
+                    .header("content-type", "application/x-www-form-urlencoded")
+                    .body(body)
+                    .asJson();
+
+            return response.getBody().getObject().get("access_token").toString();
+
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public HttpResponse<JsonNode> updateTokenWithRefresh(String refresh_token) {
+        String body = String.format("grant_type=refresh_token&" +
+                        "refresh_token=%s&" +
+                        "client_id=%s&" +
+                        "client_secret=%s&",
+                refresh_token,  env.getProperty("okta.oauth2.client-id"), env.getProperty("okta.oauth2.client-secret"));
+
+        try {
+            HttpResponse<JsonNode> response = Unirest.post("https://dev-l5yejihpj316wdj1.us.auth0.com/oauth/token")
+                    .header("content-type", "application/x-www-form-urlencoded")
+                    .body(body)
+                    .asJson();
+
+            return response;
+
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void addParamsToSession(Map session, String access_token, String refresh_token, String username) {
+        session.put("access_token", access_token);
+        session.put("header", getHeaderFromJWT(access_token));
+        session.put("payload", getPayloadFromJWT(access_token));
+        session.put("refresh_token", refresh_token);
+        session.put("username", username);
+        session.put("error", "");
+    }
+
+    public void updateAccesTokenInSession(Map session, String access_token) {
+        session.put("access_token", access_token);
+        session.put("header", getHeaderFromJWT(access_token));
+        session.put("payload", getPayloadFromJWT(access_token));
+    }
+
+    public String getHeaderFromJWT(String jwt) {
+        String codeHeader = jwt.split("\\.")[0];
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String decodeHeader = new String(decoder.decode(codeHeader));
+
+        return decodeHeader;
+    }
+
+    public String getPayloadFromJWT(String jwt) {
+        String codePayload = jwt.split("\\.")[1];
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String decodePayload = new String(decoder.decode(codePayload));
+
+        return decodePayload;
+    }
+}
