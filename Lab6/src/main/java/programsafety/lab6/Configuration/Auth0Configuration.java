5993641def9a89ffd8c89d17f8cffa90a0b0576e
Maksern <maks.men9922@gmail.com>
2024-05-10 17:18:08 +0300

    Create Auth0Configuration

---
+++ Lab6/src/main/java/programsafety/lab6/Configuration/Auth0Configuration.java
@@ -0,0 +1,55 @@
+package programsafety.lab6.Configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class Auth0Configuration {
+
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+
+    @Value("${okta.oauth2.client-id}")
+    private String clientId;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(httpRequests -> {
+                    httpRequests.requestMatchers("/home").permitAll();
+                    httpRequests.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .logout(logout -> logout
+                        .addLogoutHandler(logoutHandler()))
+                .build();
+    }
+
+
+
+    private LogoutHandler logoutHandler() {
+        return ((request, response, authentication) -> {
+            try {
+                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+}
